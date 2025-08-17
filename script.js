4ff6647d3e297ab91cf13c4b75d1633626c846c1
parbat10 <parbatlimbu139@gmail.com>
2025-08-17 15:43:59 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const todoList= [];
+
+function addTodo(){
+    const inputElement = document.querySelector('.js-todo-input');
+    const name = inputElement.value;
+    todoList.push(name);
+    console.log(todoList);
+}
