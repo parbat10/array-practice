12c4f5252f713828711564043edf534e93af859a
parbat10 <parbatlimbu139@gmail.com>
2025-08-18 16:39:38 +0545

    in process

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const todoList= [];
+const todoList= ['make dinner','wash dishes'];
 
 renderTodolist();
 
@@ -7,7 +7,15 @@ function renderTodolist(){
 
     for(let i=0; i< todoList.length; i++){
         const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+        const html = `
+                <p>
+                        ${todo}
+                        <button onclick="todoList.splice(${i},1);
+                        renderTodolist();
+                        ">delete</button>
+                        
+                    </p>
+                `;
         todoListHTML += html;
     }
     console.log(todoListHTML);
