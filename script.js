21f46754c31a0e98f79e2a2201bcdac0cbac95ef
parbat10 <parbatlimbu139@gmail.com>
2025-08-18 16:05:23 +0545

    in process

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const todoList= [];
+const todoList= ['make dinner','wash dishes'];
+
+renderTodolist();
+
+function renderTodolist(){
+    let todoListHTML = '';
+
+    for(let i=0; i< todoList.length; i++){
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`;
+        todoListHTML += html;
+    }
+    console.log(todoListHTML);
+
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML; 
+}
+
+
 
 function addTodo(){
     const inputElement = document.querySelector('.js-todo-input');
     const name = inputElement.value;
+
+
     todoList.push(name);
     console.log(todoList);
+
+    inputElement.value='';
+
+    renderTodolist();
 }
